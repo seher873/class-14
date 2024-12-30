a5faee4bbec5881fdefb9cb815f7330eb71dea00
seher873 <161340426+seher873@users.noreply.github.com>
2024-12-30 21:37:09 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,23 @@
-
 import { client } from "@/sanity/lib/client";
-interface Ibook {
-  price: "number";
-  auther: "string";
-  name: "string";
-  // slug:"slug";
-  // image:"image";
-  // heading:"string";
-  // content:"string"
 
+interface Ibook {
+  price: number; 
+  auther: string; 
+  name: string; 
+  id: string; 
 }
 
-export default async function page() {
-  let res: Ibook[] = await client.fetch("*[_type == 'book']");
+export default async function Page() {
+  const res: Ibook[] = await client.fetch("*[_type == 'book']");
 
   return (
     <div>
       {res.map((data) => {
         return (
-          <div>
-            <h1>{data.name},{data.auther},{data.price},</h1>
-            
+          <div key={data.id}> {/* Adding unique key */}
+            <h1>
+              {data.name}, {data.auther}, {data.price}
+            </h1>
           </div>
         );
       })}
--- src/sanity/book.ts
+++ src/sanity/book.ts
@@ -1,6 +1,4 @@
-import { title } from "process";
-
-export default {
+const bookSchema = {
   name: "book",
   type: "document",
   title: "book",
@@ -10,57 +8,41 @@ export default {
       type: "string",
       title: "name",
     },
-
- 
-        {
-          name: "price",
-          type: "number",
-          title: "price",
-        },
-
-
-
-        {
-            name: "auther",
-            type: "string",
-            title: "Auther",
-          },
-
-          {
-            name: "slug",
-            type: "slug",
-            title: "slug",
-            description:"the slug of book post.",
-
-          },
-
-
-          {
-            name: "image",
-            type: "image",
-            title: "image",
-            description:"the image of the auther.",
-          },
-          
-          {
-            name: "heading",
-            type: "string",
-            title: "Heading",
-            description:"the heading of the book.",
-          },
-          {
-            name: "content",
-            type: "string",
-            title: "content",
-            description:"the content of the book.",
-          },
-
-
-
+    {
+      name: "price",
+      type: "number",
+      title: "price",
+    },
+    {
+      name: "auther",
+      type: "string",
+      title: "Auther",
+    },
+    {
+      name: "slug",
+      type: "slug",
+      title: "slug",
+      description: "The slug of the book post.",
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "image",
+      description: "The image of the auther.",
+    },
+    {
+      name: "heading",
+      type: "string",
+      title: "Heading",
+      description: "The heading of the book.",
+    },
+    {
+      name: "content",
+      type: "string",
+      title: "content",
+      description: "The content of the book.",
+    },
   ],
-
-  
-  
-
-  
 };
+
+export default bookSchema;
